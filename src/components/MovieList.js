33d6c8147d7f6a12650e1827e82cf253662d8b07
Andreas Karpasitis <karpasitis@outlook.com>
2019-04-19 15:59:53 +0200

    chore: add search capability and fetch results

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -11,6 +11,7 @@ const MovieList = props => {
   const { searchTerm } = props;
   const [isSearch, setIsSearch] = useState(false);
   const [popularMovies, setPopularMovies] = useState(null);
+  const [searchMovies, setSearchMovies] = useState(null);
 
   useEffect(() => {
     const url = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=${1}`;
@@ -23,14 +24,28 @@ const MovieList = props => {
   }, []);
 
   useEffect(() => {
-    setIsSearch(searchTerm.length > 0);
+    if (searchTerm.length > 0) {
+      setIsSearch(true);
+      const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${searchTerm}&page=1&include_adult=false`
+      fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          const { results } = data;
+          setSearchMovies(results);
+        });
+    } else {
+      setSearchMovies(null);
+      setIsSearch(false);
+    }
   }, [searchTerm]);
+
+  const movies = searchMovies || popularMovies;
 
   return (
     <div className={style.container}>
       <h3>{isSearch ? 'Search Results' : 'Popular Movies'}</h3>
       <div className={style.movies}>
-        {popularMovies && popularMovies.filter(movie => movie.poster_path).map(movie => {
+        {movies && movies.filter(movie => movie.poster_path).map(movie => {
           const { id, title, poster_path } = movie; // eslint-disable-line
           const imageBaseUrl = 'https://image.tmdb.org/t/p/w500';
           return (
