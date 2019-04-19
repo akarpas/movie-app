1de8267d5056a82b153a12880c26bf82686a7914
Andreas Karpasitis <karpasitis@outlook.com>
2019-04-19 19:12:00 +0200

    fix: add semicolon

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -26,7 +26,7 @@ const MovieList = props => {
   useEffect(() => {
     if (searchTerm.length > 0) {
       setIsSearch(true);
-      const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${searchTerm}&page=1&include_adult=false`
+      const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${searchTerm}&page=1&include_adult=false`;
       fetch(url)
         .then(response => response.json())
         .then(data => {
