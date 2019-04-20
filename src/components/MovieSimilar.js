888762b44736088ce1b2ac5cfe1977b7cfedff1a
Andreas Karpasitis <karpasitis@outlook.com>
2019-04-20 21:58:37 +0200

    chore: fetch data for similar movies

--- src/components/MovieSimilar.js
+++ src/components/MovieSimilar.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { useLayoutEffect, useState } from 'react';
 
 import style from './MovieSimilar.scss';
 
-const MovieSimilar = () => {
+const API_KEY = '6e13b454073848d7b6853f415e2636be';
+const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
+
+const MovieSimilar = props => {
+  const { location } = props; // eslint-disable-line
+  const { pathname } = location;
+  const id = pathname.split('/')[2];
+  const [similarMovies, setSimilarMovies] = useState(null);
+  const [referencedMovieTitle, setReferencedMovieTitle] = useState(null);
+
+  useLayoutEffect(() => {
+    async function fetchData() {
+      const urlSimilarMovies = `https://api.themoviedb.org/3/movie/${id}/similar?api_key=${API_KEY}&language=en-US`;
+      const responseSimilarMovies = await fetch(urlSimilarMovies);
+      const dataSimilarMovies = await responseSimilarMovies.json();
+      const { results } = dataSimilarMovies;
+
+      const urlReferencedMovie = `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=en-US`;
+      const responseReferencedMovie = await fetch(urlReferencedMovie);
+      const dataReferencedMovie = await responseReferencedMovie.json();
+
+      setSimilarMovies(results);
+      setReferencedMovieTitle(dataReferencedMovie.title);
+    }
+    fetchData();
+  }, [pathname]);
+
   return (
     <div className={style.container}>
-      <div>Similar Movies</div>
+      <div>Movies Similar to:</div>
+      <h2>{referencedMovieTitle}</h2>
+      <div className={style.movies}>
+        {similarMovies && similarMovies.map(movie => (
+          <img
+            key={movie.title}
+            className={style.movieThumb}
+            src={`${IMAGE_BASE_URL}${movie.poster_path}`}
+            alt={movie.title}
+          />
+        ))}
+      </div>
     </div>
   );
 };
