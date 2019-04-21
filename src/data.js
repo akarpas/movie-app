9bf1b634fca90688938158dad7c304ff2e3797a4
Andreas Karpasitis <karpasitis@outlook.com>
2019-04-21 22:04:33 +0200

    chore: implement data file to keep most used constants relating to api calls

---
+++ src/data.js
@@ -0,0 +1,4 @@
+export const API_KEY = '6e13b454073848d7b6853f415e2636be';
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
+export const API_BASE_URL = 'https://api.themoviedb.org/3/';
+
