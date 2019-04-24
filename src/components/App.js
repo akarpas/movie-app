af7c5588cb47c819e1b497725c6b45989bbec174
Andreas Karpasitis <karpasitis@outlook.com>
2019-04-24 22:15:27 +0200

    chore: implement page transition with 500 timeout

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import debounce from 'lodash.debounce';
-import { AnimatedSwitch } from 'react-router-transition';
+import PageTransition from 'react-router-page-transition';
 import MovieList from './MovieList';
 import MoviePage from './MoviePage';
 import MovieSimilar from './MovieSimilar';
 
 import style from './App.scss';
+import './transition.scss';
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
@@ -28,17 +29,17 @@ const App = () => {
         />
       </header>
       <MovieList searchTerm={searchTerm} />
-      <Switch>
-        <AnimatedSwitch
-          atEnter={{ opacity: 0.25 }}
-          atLeave={{ opacity: 0.25 }}
-          atActive={{ opacity: 1 }}
-          className="switch-wrapper"
-        >
-          <Route path="/movie/:id" component={MoviePage} />
-          <Route path="/similar/:id" component={MovieSimilar} />
-        </AnimatedSwitch>
-      </Switch>
+      <Route
+        render={({ location }) => (
+          <PageTransition timeout={500}>
+            <Switch location={location}>
+              <Route exact path="/" component={() => <div></div>}/>
+              <Route path="/movie/:id" component={MoviePage} />
+              <Route path="/similar/:id" component={MovieSimilar} />
+            </Switch>
+          </PageTransition>
+        )}
+      />
     </div>
   );
 };
